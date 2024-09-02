20c08d24d6c41d53bade638a90a4e702c52abcba
sriram <sriramsvne@gmail.com>
2024-09-03 03:06:55 +0530

    feat: add DataType.java

---
+++ src/com/sriram/java/DataTypes.java
@@ -0,0 +1,51 @@
+package com.sriram.java;
+
+public class DataTypes {
+    public static void main(String[] args) {
+
+        /*
+        WORKING WIT PRIMITIVES
+         */
+
+        byte age;
+        age=50;
+        System.out.println("AGE : " + age);
+
+        short orderValue;
+        orderValue=5000;
+        System.out.println("ORDER VALUE : " + orderValue);
+
+        int salary;
+        salary=45000;
+        System.out.println("SALARY : " + salary);
+
+        long monthlySalary;
+        monthlySalary=750000L;
+        System.out.println("MONTHLYSALARY : " + monthlySalary);
+
+        float avgSalary;
+        avgSalary=784.89F;
+        System.out.println("AVERAGESALARY : " + avgSalary);
+
+        double totalAverageSalary;
+        totalAverageSalary=893324.87794;
+        System.out.println("TOTALAVERAGESALARY : " + totalAverageSalary);
+
+        char gender;
+        gender='F';
+        System.out.println("GENDER : " + gender);
+
+        boolean isEliglible;
+        isEliglible=true;
+        System.out.println("IS ELIGLIBLE : " + isEliglible);
+
+
+
+
+
+
+    }
+
+
+}
+
